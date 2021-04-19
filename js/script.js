8b9aea884688acd09a923e8b63ec4abc923aa6e1
Simone <marzolla.istruzione@gmail.com>
2021-04-19 17:18:31 +0200

    Come si sostituiscono le bandiere e le stelline?

--- js/script.js
+++ js/script.js
@@ -2,29 +2,29 @@ function init() {
   new Vue({
     el: '#app',
     data: {
-      search: 'a',
+      search: '',
       films: []
     },
-    mounted() {
-      axios.get('https://api.themoviedb.org/3/search/movie', {
-            params: {
-              api_key: 'd474fa6ed1e989704b60543e0259c030',
-              query: this.search.toUpperCase()
-            }
-          })
-         .then(data => {
-            console.log(data);
-            this.films = data.data.results;
-         })
-       },
     methods: {
       clicked: function() {
-        console.log("Lista film");
-        for(let i = 0; i < films.length; i++) {
-          if(this.search == this.films[i]) {
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+              params: {
+                api_key: 'd474fa6ed1e989704b60543e0259c030',
+                query: this.search.toUpperCase()
+              }
+            }
+        )
+        .then(data => {
+          this.films = data.data.results;
+          console.log(data.data.results);
+          switch (this.film.original_language) {
+            case "en":
+              this.films.original_language = "U+1F3F4";
+              break;
+            default:
 
           }
-        }
+        })
       }
     }
   });
