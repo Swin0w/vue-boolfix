d345e7c0bb298016ed3f3edc5b1f61871374b9e7
Simone <marzolla.istruzione@gmail.com>
2021-04-19 16:35:00 +0200

    push

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 function init() {
   new Vue({
+    el: '#app',
+    data: {
+      search: 'a',
+      films: []
+    },
     mounted() {
-      axios.get('', {
-        params: {
-          api_key: '',
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+              api_key: 'd474fa6ed1e989704b60543e0259c030',
+              query: this.search.toUpperCase()
+            }
+          })
+         .then(data => {
+            console.log(data);
+            this.films = data.data.results;
+         })
+       },
+    methods: {
+      clicked: function() {
+        console.log("Lista film");
+        for(let i = 0; i < films.length; i++) {
+          if(this.search == this.films[i]) {
+
+          }
         }
-      })
+      }
     }
   });
 }
