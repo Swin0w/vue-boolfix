77a00da710135fe4619dd20459b2d87d19146616
Simone <marzolla.istruzione@gmail.com>
2021-04-20 17:57:39 +0200

    Esercizio concluso

--- js/script.js
+++ js/script.js
@@ -6,13 +6,18 @@ function init() {
       films: [],
       series: [],
       flags: {
-        it: "flag/it_flag.png",
-        es: "flag/es_flag.png",
-        en: "flag/en_flag.png",
-        ja: "flag/jp_flag.png",
-        fr: "flag/fr_flag.png"
+        it: "img/flag/it_flag.png",
+        es: "img/flag/es_flag.png",
+        en: "img/flag/en_flag.png",
+        ja: "img/flag/jp_flag.png",
+        fr: "img/flag/fr_flag.png"
       },
       isInArray: false
+    },
+    computed: {
+      allFilms: function() {
+        return [...this.films, ...this.series]; //Creazione di un array unico tramite ...
+      }
     },
     methods: {
       clicked: function() {
@@ -25,7 +30,7 @@ function init() {
         )
         .then(data => {
           this.films = data.data.results
-          console.log(data.data.results);
+          console.log("Film" , data.data.results);
         })
         axios.get('https://api.themoviedb.org/3/search/tv', {
               params: {
@@ -36,11 +41,10 @@ function init() {
          )
          .then(data=> {
            this.series = data.data.results;
-           console.log(data.data.results);
+           console.log("Serie tv", data.data.results);
          })
       },
       convertMarksToStars: function(marks) {
-        console.log(marks);
         marks = parseInt(marks / 2);
         return marks;
       },
